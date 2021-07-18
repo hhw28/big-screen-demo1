94d3aa51ef3f1b0dc76e87b0857aeabc0b0bf686
wanghuan <196912334@qq.com>
2021-07-18 17:24:39 +0800

    柱形图1

--- src/components/chart-1.tsx
+++ src/components/chart-1.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
+import * as echarts from 'echarts';
+import {createEchartsOptions} from '../shared/create-echarts-options';
 
 export const Chart1 = () => {
+  const divRef = useRef(null)
+
+  useEffect(() => {
+    const myChart = echarts.init(divRef.current);
+    myChart.setOption(createEchartsOptions({
+      xAxis: {
+        data: ['兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区'],
+        axisTick: {show: false},
+        axisLine: {
+          lineStyle: {color: '#083B70'}
+        },
+        axisLabel: {
+          formatter(val) {
+            if (val.length > 2) {
+              const array = val.split('');
+              array.splice(2, 0, '\n');
+              return array.join('');
+            } else {
+              return val;
+            }
+          }
+        },
+      },
+
+      yAxis: {
+        splitLine: {show: false},
+        axisLine: {
+          show: true,
+          lineStyle: {color: '#083B70'}
+        }
+      },
+      series: [{
+        type: 'bar',
+        data: [10, 20, 36, 41, 15, 26, 37, 18, 29]
+      }]
+    }));
+  }, [])
 
   return (
     <div className="bordered chart1">
       <h2>柱形图1</h2>
-      <div className="chart"></div>
+      <div ref={divRef} className="chart"></div>
     </div>
   );
 };
